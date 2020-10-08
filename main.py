8007047771a565be40b046d49c8608c2bfe1639c
Josh Wolsborn <josh.wolsborn@seeq.com>
2020-10-07 17:09:36 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+
+player1 = input("What's your name, Player 1? ")
+player2 = input("And your name, Player 2? ")
+
+player1_answer = input("%s, do you want to choose rock, paper or scissors? " % player1)
+player2_answer = input("%s, do you want to choose rock, paper or scissors? " % player2)
+
+
+
+def compare(u1, u2):
+    if u1 == u2:
+        return("") #What message should you return here if both players give the same answer?
+    elif u1 == 'rock':
+        if u2 == 'scissors':
+            return("") #What message should you return if answer1 is rock and answer2 is scissors?
+        else:
+            return("") #Hmmm this one may be tricky...what answer would you return here?
+    elif u1 == 'scissors':
+        if u2 == 'paper':
+            return("") #What message would you return if answer1 is scissors and answer2 is paper
+        else:
+            return("") #What answer would you return here
+    
+    #We need one more conditional block like the 2 above see if you can figure it out
+
+
+
+#Finally we need to call our function inside this print statement with the answers given by the users.  What would we put in the print statement?
+print()
